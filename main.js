3a7647f9b197446e43de1ff5753e1bd320ae34b7
Daniil <daniil.tarasonis@gmail.com>
2025-04-15 19:37:00 +0300

    Added the ability to keep track of stats

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ import { fetchRandomPoem } from './fetch_data.js';
 
 const textContainer = document.getElementById('text-to-type')
 const inputContainer = document.getElementById('input-field')
+const wpmElement = document.getElementById('wpm')
+const accuracyElement = document.getElementById('accuracy')
+let correctChars = 0
+let totalKeyPresses = 0
+let totalTypedChars = 0
+let startTime = null
 
 function displayParagraph(paragraph) {
   if (!textContainer) {
@@ -23,9 +29,23 @@ function displayParagraph(paragraph) {
   })
 }
 
-function randomFunction() {
+function updateStats() {
+  if (!startTime) return;
+
+  const elapsedTimeInMinutes = (Date.now() - startTime) / 1000 / 60
+  const wpm = Math.round((correctChars / 5) / elapsedTimeInMinutes)
+  const accuracy = Math.round((correctChars / totalKeyPresses) * 100) || 0
+
+  wpmElement.innerText = wpm
+  accuracyElement.innerText = `${accuracy}%`
+}
+
+function textAnalyzer() {
   const arrayText = textContainer.querySelectorAll('span')
   const arrayValue = inputContainer.value.split('')
+
+  correctChars = 0
+
   arrayText.forEach((characterSpan, index) => {
     const character = arrayValue[index]
     if (character == null) {
@@ -34,22 +54,35 @@ function randomFunction() {
     } else if (character === characterSpan.innerText) {
       characterSpan.classList.add('correct')
       characterSpan.classList.remove('incorrect')
+      correctChars++
     } else {
       characterSpan.classList.remove('correct')
       characterSpan.classList.add('incorrect')
     }
   })
+
+  updateStats()
 }
+
 
 window.onload = async () => {
   const fetchedText = await fetchRandomPoem();
   displayParagraph(fetchedText)
 
   document.getElementById('input-field').addEventListener('keydown', () => {
-    startTimer()
+    if (!startTime) {
+      startTime = Date.now()
+      startTimer()
+    }
   })
 
-  inputContainer.addEventListener('input', () => {
-    randomFunction()
-  })
+  inputContainer.addEventListener('input', (e) => {
+    const inputType = e.inputType;
+  
+    if (inputType !== 'deleteContentBackward') {
+      totalKeyPresses++;
+    }
+  
+    textAnalyzer();
+  });
 }
